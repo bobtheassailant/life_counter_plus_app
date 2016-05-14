61d0f7929c1f989ab8865b80534aa2c67cc5f51d
Louie Williford <louie@Louies-MacBook.local>
2016-05-14 16:48:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/louie/lifecounter/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.louie.lifecounter;
+
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View lFragment =  findViewById(R.id.left_fragment);
+        View rFragment =  findViewById(R.id.right_fragment);
+
+
+        lFragment.setBackgroundColor(Color.RED);
+        rFragment.setBackgroundColor(Color.GREEN);
+
+
+
+
+    }
+
+
+
+}
